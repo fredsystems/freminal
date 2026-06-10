6a47d9f1249b8709fa6c441b743b86303e23d751
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2026-06-10 10:45:06 -0600

    chore(version): update version for next beta

--- flake.nix
+++ flake.nix
@@ -100,7 +100,7 @@
         {
           freminal = rustPlatform.buildRustPackage {
             pname = "freminal";
-            version = "0.9.0-beta.2";
+            version = "0.9.0-beta.3";
             src = pkgs.lib.cleanSource ./.;
 
             cargoLock.lockFile = ./Cargo.lock;
@@ -148,9 +148,9 @@
                     <key>CFBundleIdentifier</key>
                     <string>io.github.fredclausen.freminal</string>
                     <key>CFBundleVersion</key>
-                    <string>0.9.0-beta.2</string>
+                    <string>0.9.0-beta.3</string>
                     <key>CFBundleShortVersionString</key>
-                    <string>0.9.0-beta.2</string>
+                    <string>0.9.0-beta.3</string>
                     <key>CFBundleExecutable</key>
                     <string>freminal</string>
                     <key>CFBundleIconFile</key>
